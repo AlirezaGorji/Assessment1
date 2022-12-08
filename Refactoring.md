eeadab5c566d114eace945532d91529e05294fc9
Alireza Gorji <alireza.gorji@zwapgrid.com>
2022-12-07 21:14:59 -0800

    added new function for refactoring. Added explanation for refactoring

--- Refactoring.md
+++ Refactoring.md
@@ -9,3 +9,5 @@ You've been asked to refactor the function `deterministicPartitionKey` in [`dpk.
 You will be graded on the exhaustiveness and quality of your unit tests, the depth of your refactor, and the level of insight into your thought process provided by the written explanation.
 
 ## Your Explanation Here
+I tried to eliminatte many if/else statements so it is more readable.
+Since crypto.createHash used twice I place it into a new function and passed it the actual data to be hashed.
--- dpk.js
+++ dpk.js
@@ -7,11 +7,14 @@ exports.deterministicPartitionKey = (event) => {
 
   const data = event ? JSON.stringify(event) : null;
 
-  candidate = event ? event.partitionKey ? event.partitionKey : crypto.createHash("sha3-512").update(data).digest("hex") : candidate
+  candidate = event ? event.partitionKey ? event.partitionKey : createHashFromData(data) : candidate
 
   candidate =  candidate ? typeof candidate !== "string" ? JSON.stringify(candidate) : candidate : TRIVIAL_PARTITION_KEY;
 
-  candidate = candidate.length > MAX_PARTITION_KEY_LENGTH ? crypto.createHash("sha3-512").update(candidate).digest("hex") : candidate;
+  candidate = candidate.length > MAX_PARTITION_KEY_LENGTH ? createHashFromData(candidate) : candidate;
   return candidate;
 };
 
+const createHashFromData = (data) => {
+  return crypto.createHash("sha3-512").update(data).digest("hex")
+}
